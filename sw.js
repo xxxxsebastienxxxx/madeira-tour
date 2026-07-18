34782de9cb224f19f6fe805160717628f311ccd8
xxxxsebastienxxxx <xxxxsebastienxxxx@gmail.com>
2026-07-19 00:33:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,27 @@
-const CACHE='madeira-v4';
-const ASSETS=['./','./index.html','./leaflet-local.css?v=4','./styles-v4.css?v=4','./app-v4.js?v=4','./data.json','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+const CACHE='madeira-v5';
+const ASSETS=[
+  './','./index.html','./leaflet-local.css?v=5','./styles-v5.css?v=5','./app-v5.js?v=5',
+  './data.json?v=5','./manifest.webmanifest','./icon-192.svg','./icon-512.svg','./photo-fallback.svg'
+];
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys()
+    .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+    .then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',event=>{
-  const request=event.request;
-  if(request.method!=='GET') return;
-  const url=new URL(request.url);
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
   if(url.origin===location.origin){
-    event.respondWith(
-      fetch(request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put(request,copy));
-          return response;
-        })
-        .catch(()=>caches.match(request).then(r=>r||caches.match('./index.html')))
-    );
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
   }else{
-    event.respondWith(fetch(request).catch(()=>caches.match(request)));
+    event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
   }
 });
