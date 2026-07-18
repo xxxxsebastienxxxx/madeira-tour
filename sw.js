0a914647bc53fce7eb61f7b117498d88af29e254
xxxxsebastienxxxx <xxxxsebastienxxxx@gmail.com>
2026-07-18 19:26:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,31 @@
-const CACHE='madeira-v3';const ASSETS=['./','./index.html','./styles.css','./app.js','./data.json','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='madeira-v4';
+const ASSETS=['./','./index.html','./leaflet-local.css?v=4','./styles-v4.css?v=4','./app-v4.js?v=4','./data.json','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET') return;
+  const url=new URL(request.url);
+  if(url.origin===location.origin){
+    event.respondWith(
+      fetch(request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(request).then(r=>r||caches.match('./index.html')))
+    );
+  }else{
+    event.respondWith(fetch(request).catch(()=>caches.match(request)));
+  }
+});
